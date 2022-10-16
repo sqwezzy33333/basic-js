30fcadddf07f762abc3c87b49feb0c10d271d60a
Sqwezzy33333 <sergey.kornev3@yandex.by>
2022-10-16 13:38:38 +0300

    dream-team2

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
-module.exports = {
-  repeater
-};
+ function repeater(str, opt) {
+   str = String(str);
+ 
+   let options = {
+     repeatTimes: ('repeatTimes' in opt && typeof(opt.repeatTimes) === 'number') ? opt.repeatTimes : 1, // кол-во повторений строки
+     separator: 'separator' in opt ? opt.separator : '+', // разделитель строки
+     addition: 'addition' in opt ? String(opt.addition) : '', // допополнение к строке
+     additionRepeatTimes: ('additionRepeatTimes' in opt && typeof(opt.additionRepeatTimes) === 'number') ? opt.additionRepeatTimes : 1, // кол-во дополнений к строке
+     additionSeparator: 'additionSeparator' in opt ? opt.additionSeparator : '|' // разделитель дополлнений к строке
+   };
+ 
+   let extra = '';
+   if (options.additionRepeatTimes > 0) {
+     extra = options.addition;
+     for (let i = 1; i < options.additionRepeatTimes; i++) {
+       extra += options.additionSeparator + options.addition
+     }
+   }
+ 
+   let message = str + extra;
+   for (let i = 1; i < options.repeatTimes; i++) {
+       message += options.separator + str + extra;
+   }
+   
+   return message;
+ };
+
+ module.exports = {
+   repeater
+ };
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function transform(arr) {
+   if (Array.isArray(arr) == false) {
+     throw Error("arr' parameter must be an instance of the Array!");
+   }
+   
+   let newArray = [];
+   let toggle = true;
+   arr.forEach(function (item, i) {
+     
+
+     if (item === '--discard-next') { 
+       toggle = false;
+     } else if (item === '--discard-prev') {
+       newArray.pop();
+     } else if (item === '--double-next') { 
+       if (i < arr.length - 1) {
+         newArray.push(arr[i+1]);
+       }
+     } else if (item === '--double-prev') { 
+       if (i > 0) {
+         newArray.push(arr[i-1]);
+       }
+     } else if (toggle) {
+       newArray.push(item);
+     } else {
+       toggle = true;
+     }
+   });
+ 
+   return newArray;
+ };
 
 module.exports = {
   transform
